b09f8465b978d0a850d0328ae29eb054c01f5d33
manuel <manue@LENOVOPC>
2023-09-09 15:27:08 +0200

    Aggiungere i file di progetto.

---
+++ EsRientrofcs/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace EsRientrofcs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.aggiusta = new System.Windows.Forms.Button();
+            this.ConCampi = new System.Windows.Forms.Button();
+            this.LMaxRec = new System.Windows.Forms.Button();
+            this.AggRecord = new System.Windows.Forms.Button();
+            this.VisCampi = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "GESTIONE FILE CSV";
+            // 
+            // aggiusta
+            // 
+            this.aggiusta.Location = new System.Drawing.Point(18, 47);
+            this.aggiusta.Name = "aggiusta";
+            this.aggiusta.Size = new System.Drawing.Size(130, 23);
+            this.aggiusta.TabIndex = 1;
+            this.aggiusta.Text = "Aggiusta";
+            this.aggiusta.UseVisualStyleBackColor = true;
+            this.aggiusta.Click += new System.EventHandler(this.aggiusta_Click);
+            // 
+            // ConCampi
+            // 
+            this.ConCampi.Location = new System.Drawing.Point(18, 77);
+            this.ConCampi.Name = "ConCampi";
+            this.ConCampi.Size = new System.Drawing.Size(130, 23);
+            this.ConCampi.TabIndex = 2;
+            this.ConCampi.Text = "Conta Campi";
+            this.ConCampi.UseVisualStyleBackColor = true;
+            this.ConCampi.Click += new System.EventHandler(this.ConCampi_Click);
+            // 
+            // LMaxRec
+            // 
+            this.LMaxRec.Location = new System.Drawing.Point(18, 107);
+            this.LMaxRec.Name = "LMaxRec";
+            this.LMaxRec.Size = new System.Drawing.Size(130, 23);
+            this.LMaxRec.TabIndex = 3;
+            this.LMaxRec.Text = "Lunghezza Massima";
+            this.LMaxRec.UseVisualStyleBackColor = true;
+            this.LMaxRec.Click += new System.EventHandler(this.LMaxRec_Click);
+            // 
+            // AggRecord
+            // 
+            this.AggRecord.Location = new System.Drawing.Point(18, 137);
+            this.AggRecord.Name = "AggRecord";
+            this.AggRecord.Size = new System.Drawing.Size(130, 23);
+            this.AggRecord.TabIndex = 4;
+            this.AggRecord.Text = "Aggiunta Record";
+            this.AggRecord.UseVisualStyleBackColor = true;
+            this.AggRecord.Click += new System.EventHandler(this.AggRecord_Click);
+            // 
+            // VisCampi
+            // 
+            this.VisCampi.Location = new System.Drawing.Point(18, 167);
+            this.VisCampi.Name = "VisCampi";
+            this.VisCampi.Size = new System.Drawing.Size(130, 23);
+            this.VisCampi.TabIndex = 5;
+            this.VisCampi.Text = "Visualizza tre campi";
+            this.VisCampi.UseVisualStyleBackColor = true;
+            this.VisCampi.Click += new System.EventHandler(this.VisCampi_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(184, 63);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(595, 362);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.VisCampi);
+            this.Controls.Add(this.AggRecord);
+            this.Controls.Add(this.LMaxRec);
+            this.Controls.Add(this.ConCampi);
+            this.Controls.Add(this.aggiusta);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button aggiusta;
+        private System.Windows.Forms.Button ConCampi;
+        private System.Windows.Forms.Button LMaxRec;
+        private System.Windows.Forms.Button AggRecord;
+        private System.Windows.Forms.Button VisCampi;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ EsRientrofcs/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using libreriaCS;
+
+namespace EsRientrofcs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aggiusta_Click(object sender, EventArgs e)
+        {
+            LibreriaCS l = new LibreriaCS();
+            if (l.ContrAgg() == false)
+            {
+                l.aggiusta();
+                MessageBox.Show("File aggiustato correttamente", "Avviso", MessageBoxButtons.OK);
+            }
+            else
+            {
+                MessageBox.Show("Operazione già effettuata", "Avviso", MessageBoxButtons.OK);
+            }
+        }
+
+        private void ConCampi_Click(object sender, EventArgs e)
+        {
+            LibreriaCS l = new LibreriaCS();
+            MessageBox.Show("I campi di questo file sono " + l.ContaCampi() + ".", "Numero dei campi", MessageBoxButtons.OK);
+        }
+
+        private void LMaxRec_Click(object sender, EventArgs e)
+        {
+            LibreriaCS l = new LibreriaCS();
+            MessageBox.Show("La lunghezza massima dei record presenti è " + l.LungMaxRec() + " caratteri.", "Lunghezza Massima Record", MessageBoxButtons.OK);
+        }
+
+        private void AggRecord_Click(object sender, EventArgs e)
+        {
+            LibreriaCS l = new LibreriaCS();
+            string[] div = new string[18];
+            MessageBox.Show("Inserisci i campi del nuovo record che vuoi inserire:", "Aggiunta di un nuovo record");
+            div[0] = Interaction.InputBox("Inserisci il Comune:", "Nuovo record");
+            div[1] = Interaction.InputBox("Inserisci la Provincia:", "Nuovo record");
+            div[2] = Interaction.InputBox("Inserisci la Regione:", "Nuovo record");
+            div[3] = Interaction.InputBox("Inserisci la Tipologia:", "Nuovo record");
+            div[4] = Interaction.InputBox("Inserisci la Categoria:", "Nuovo record");
+            div[5] = Interaction.InputBox("Inserisci la Denominazione:", "Nuovo record");
+            div[6] = Interaction.InputBox("Inserisci l'Indirizzo:", "Nuovo record");
+            div[7] = Interaction.InputBox("Inserisci il CAP:", "Nuovo record");
+            div[8] = Interaction.InputBox("Inserisci la Località:", "Nuovo record");
+            div[9] = Interaction.InputBox("Inserisci la Frazione:", "Nuovo record");
+            div[10] = Interaction.InputBox("Inserisci il Telefono:", "Nuovo record");
+            div[11] = Interaction.InputBox("Inserisci il FAX:", "Nuovo record");
+            div[12] = Interaction.InputBox("Inserisci l'Indirizzo posta elettronica:", "Nuovo record");
+            div[13] = Interaction.InputBox("Inserisci il Sito Internet:", "Nuovo record");
+            div[14] = Interaction.InputBox("Inserisci il Codice esercizio:", "Nuovo record");
+            div[15] = Interaction.InputBox("Inserisci il numero di Camere:", "Nuovo record");
+            div[16] = Interaction.InputBox("Inserisci i Posti letto standard:", "Nuovo record");
+            div[17] = Interaction.InputBox("Inserisci i Posti letto aggiuntivi:", "Nuovo record");
+            l.AggRec(div);
+            MessageBox.Show("Record aggiunto correttamente", "Avviso");
+        }
+
+        private void VisCampi_Click(object sender, EventArgs e)
+        {
+            LibreriaCS l = new LibreriaCS();
+            int[] cvis = new int[3];
+            Interaction.InputBox("0. Comune" +
+                "\n1. Provincia" +
+                "\n2. Regione" +
+                "\n3. Tipologia" +
+                "\n4. Categoria(Stelle)" +
+                "\n5. Denominazione" +
+                "\n6. Indirizzo" +
+                "\n7. CAP" +
+                "\n8. Località" +
+                "\n9. Frazione" +
+                "\n10. Telefono" +
+                "\n11. FAX" +
+                "\n12. Indirizzo Posta Elettronica" +
+                "\n13. Sito Internet" +
+                "\n14. Codice esercizio" +
+                "\n15. Camere" +
+                "\n16. Posti letto standard" +
+                "\n17. Posti letto aggiuntivi", "Visualizza 3 Campi");
+           /* for (int i = 0; i < cvis.Length; i++)
+            {
+                "Inserisci il numero:");
+                cvis[i] = int.Parse(Console.ReadLine());
+            }
+            string[] arr = l.EstrapolaCampi(cvis[0], cvis[1], cvis[2]);
+            int j = 0;
+            while (arr[j] != null)
+            {
+                arr[j]);
+                j++;
+            }
+            "Clicca un tasto per continuare...");
+            Console.ReadLine();*/
+        }
+    }
+}
